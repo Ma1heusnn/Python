0b523b59c7d5ae8ee0466ed270cf6c0639eab713
Matheus Natale <matheusnatale2009@gmail.com>
2025-08-18 19:27:43 -0300

    Comentários do conversor.py

--- conversor.py
+++ conversor.py
@@ -1,5 +1,5 @@
 import math
-
+#Esse código tem intuito de poder escolher qualquer tipo de número, independentemente da sua base e transformar em um número de qualquer outra base à preferência do usuário
 def escrita():
     while True:     
         numero = input("Digite o número que deseja transformar: ")
@@ -13,6 +13,7 @@ def escrita():
         if base not in [16, 10, 8, 2]:
             print("Digite uma base válida!")
             continue
+            #Esse try tem função de realizar a operação e, caso haja erro, há o salto direto para a seção do except, onde realizamos o retorno para o começo. Caso a operação seja realizada com sucesso, será realizado o break para sair do loop
         try:
             valor_decimal = int(numero, base)
             break
@@ -30,7 +31,7 @@ def saida(valor_decimal):
                            
         R: """))
         if desejo == 16:
-            resultado = hex(valor_decimal)[2:]
+            resultado = hex(valor_decimal)[2:] #Usamos o "[2:]" pois nas operações de hex, oct e bin, são gerados dois caracteres antes do resultado, dessa forma, podemos pulá-los e exibir unicamente aquilo que nos importa
         elif desejo == 10:
             resultado = int(valor_decimal)
         elif desejo == 8:
@@ -42,7 +43,8 @@ def saida(valor_decimal):
             continue
         return resultado, desejo
 
-# Execução principal
+# Execução principal serve para processarmos os dados para que o sistema consiga realizar sua impressão na tela.
 valor_decimal, numero, base = escrita()
 resultado, desejo = saida(valor_decimal)
 print(f"Seu número ({numero}) de base {base} foi convertido para um número de base {desejo} e é equivalente a {resultado}")
+
