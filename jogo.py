d3f3628e6210d81582c791b0eca09b31285945c4
Matheus Natale <matheusnatale2009@gmail.com>
2025-08-24 12:41:19 -0300

    Jogo da velha realizado em python!
    Realizei a criação desse jogo da velha através de uma aula que assisti, ao final, retirei todas as minhas dúvidas referente ao código no Copilot para melhor absorção do conteúdo. Esse programa me serviu de aprendizado quanto ao uso de parâmetros e suas respectivas nomenclaturas, além de condições e contagem em python

---
+++ jogo.py
@@ -0,0 +1,61 @@
+"""
+Jogo da Velha em Python!
+"""
+
+#   Realiza a exibição do tabuleiro com "|" cortando entre os números
+def mostrar_tabuleiro(tabuleiro):
+    for linha in tabuleiro:
+        print(" | " .join(linha))
+
+#   Checa se houve vencedor ou não
+def verificar(tabuleiro, jogador):
+    #checagem linhas
+    for i in range(3):
+        if tabuleiro[i][0] == jogador and tabuleiro[i][1] == jogador and tabuleiro[i][2] == jogador:
+            return True
+        
+    #checagem colunas
+    for i in range(3):
+        if tabuleiro[0][i] == jogador and tabuleiro[1][i] == jogador and tabuleiro[2][i] == jogador:
+            return True
+        
+    #checagem diagonais
+    for i in range(3):
+        if tabuleiro[0][0] == jogador and tabuleiro[1][1] == jogador and tabuleiro[2][2] == jogador:
+            return True
+        if tabuleiro[0][2] == jogador and tabuleiro[1][1]  == jogador and tabuleiro[2][0] == jogador:
+            return True
+        
+    return False
+
+tabuleiro = [["1", "2", "3"],
+             ["4", "5", "6"],
+             ["7", "8", "9"]]
+jogador_atual = "X"
+
+#   Define o número de rodadas a serem jogadas e a posição escolhida pelo jogador
+for rodada in range(9):
+    mostrar_tabuleiro(tabuleiro)
+    escolha = input(f"Jogador {jogador_atual}, digite a posição que deseja jogar (1-9): ")
+    pos = int(escolha) - 1
+    linha, coluna = pos // 3, pos % 3 
+
+    if tabuleiro[linha][coluna] in ["X", "O"]:
+        print("Posição já ocupada; tente outra.")
+        continue
+
+    tabuleiro[linha][coluna] = jogador_atual
+
+    #   Realiza a troca entre os jogadores
+    if jogador_atual == "O":
+        jogador_atual = "X"
+    else:
+        jogador_atual = "O"
+
+    #   Verifica se houve vencedor ou não
+    if verificar(tabuleiro, jogador_atual):
+        mostrar_tabuleiro(tabuleiro)
+        print(f"Jogador {jogador_atual} venceu!")
+        break
+
+   
